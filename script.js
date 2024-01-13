985a35030bdefe324351c9fc5c50a39c8a42741d
Rajasimman-R <131901523+Rajasimman-R@users.noreply.github.com>
2024-01-13 13:07:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const extendHex = (shortHex) => {
-  // write your code here
 	var fullHex="#";
-	for(let i=0;i<3;i++)
+  // write your code here
+		if(shortHex.charAt(0)==="#"){
+			for(let i=1;i<=3;i++)
 	{
 		for(let j=1;j<=2;j++)
 		{
 			fullHex+=shortHex.charAt(i);
 		}
 	}
-	return fullHex;
+		}else{
+			for(let i=0;i<3;i++)
+	{
+		for(let j=1;j<=2;j++)
+		{
+			fullHex+=shortHex.charAt(i);
+		}
+	}
+		}
+	return fullHex; 
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
+ const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
+ 
